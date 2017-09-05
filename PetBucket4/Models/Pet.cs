6e091648cb85bace21eedfc89e4f247a2af94c31
Isaac S <31638709+iziin@users.noreply.github.com>
2017-09-05 11:48:37 +1000

    START
    start

---
+++ PetBucket4/Models/Pet.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PetBucket4.Models
+{
+    public class Pet
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Animal { get; set; }
+        public string Size { get; set; }
+        public string Food { get; set; }
+        public string Photo { get; set; }
+        public int CustomerId { get; set; }
+
+
+
+
+    }
+}
