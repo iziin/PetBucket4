152f92512f6458e84186f3de0686f2aac1390d9c
wichaelmilson <c3114828@uon.edu.au>
2017-09-09 23:07:06 +1000

    Made this commit for SQL Queries, And added view to view folders called booking and edited booking page to have asp

---
+++ PetBucket4/Controllers/BookingController.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PetBucket4.Models;
+using System.Data.Entity.Validation;
+using System.Linq;
+
+namespace PetBucket4.Controllers
+{
+    public class BookingController : Controller
+    {
+        private PetBucketDatabaseEntities db = new PetBucketDatabaseEntities();
+
+        // GET: Booking
+        public ActionResult Index()
+        {
+            return View(db.Pets.ToList());
+        }
+
+        // GET: Booking/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pet pet = db.Pets.Find(id);
+            if (pet == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pet);
+        }
+
+        // GET: Booking/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Booking/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,name,animal,size,food,photo,customer_id,active,indoors_safe,under13_safe")] Pet pet)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Pets.Add(pet);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(pet);
+        }
+
+        // GET: Booking/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pet pet = db.Pets.Find(id);
+            if (pet == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pet);
+        }
+
+        // POST: Booking/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,name,animal,size,food,photo,customer_id,active,indoors_safe,under13_safe")] Pet pet)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(pet).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(pet);
+        }
+
+        // GET: Booking/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pet pet = db.Pets.Find(id);
+            if (pet == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pet);
+        }
+
+        // POST: Booking/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Pet pet = db.Pets.Find(id);
+            db.Pets.Remove(pet);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+       
+    }
+}
---
+++ SQLQuery.sql
@@ -0,0 +1,61 @@
+﻿use [\\MAC\HOME\DESKTOP\I\GITHUB\PETBUCKET4\PETBUCKET4\APP_DATA\PETBUCKETDATABASE.MDF]
+select * from Pets;
+select * from Customer;
+select * from Appointment;
+
+insert into Pets (name, animal, size, food, photo, active, indoors_safe, under13_safe)
+values ('gary', 'dog', 'large', 'dog food', 'N/A', 'yes','yes','yes');
+
+select name, animal from Pets WHERE active='yes' ;
+
+
+select name from Pets WHERE animal='dog' ;
+
+select name, animal from Pets WHERE size = 'large' AND indoors_safe = 'no'; 
+
+
+select name, animal from Pets WHERE size = 'small' AND indoors_safe = 'yes'; 
+
+
+
+
+
+-- shows name of animals witht e price and start date for appointment
+SELECT Pets.id, Pets.name, Appointment.price, Appointment.date_start
+FROM Appointment
+INNER JOIN Pets ON Appointment.id = Pets.id;
+
+-- Show customers and dogs
+SELECT Customer.given_name, Customer.surname, Pets.animal, Pets.name
+From Customer
+INNER JOIN Pets ON Customer.id = Pets.id;
+
+-- Show customers who have picked up their dogs between 2 dates
+SELECT Customer.given_name, Customer.surname, Pets.animal, Pets.name, Appointment.date_end
+From Customer
+INNER JOIN Pets ON Customer.id = Pets.id
+INNER JOIN Appointment ON Customer.id = Appointment.id
+WHERE date_end BETWEEN 07/04/2017 AND 09/09/2017;
+
+--Show customers with premium service on a specific date
+SELECT Pets.name, Appointment.service
+From Appointment
+INNER JOIN Pets ON Appointment.id = Pets.id
+WHERE date_end = 09/09/2017;
+
+-- Show customers who have spent over 300
+SELECT Customer.given_name, Customer.surname, Appointment.price
+FROM Customer
+INNER JOIN Appointment ON Customer.id = Appointment.id
+WHERE Appointment.price >= 300;
+
+-- Show details for customer that requires pickup
+SELECT Customer.given_name, Customer.surname, Appointment.date_start, Appointment.needs_pickup,
+Appointment.pickup_distance, Pets.name
+FROM Appointment
+INNER JOIN Pets ON Appointment.id = Pets.id
+INNER JOIN Customer ON Appointment.id = Customer.id
+WHERE Appointment.needs_pickup = 'yes';
+
+
+--
